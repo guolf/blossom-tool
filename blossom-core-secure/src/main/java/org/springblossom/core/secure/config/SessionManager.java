e3141c5b22714f3a52b26cc64b58314230c1a541
guo <312664801@qq.com>
2020-04-30 11:29:30 +0800

    限流组件

--- blossom-core-secure/src/main/java/org/springblossom/core/secure/config/SessionManager.java
+++ blossom-core-secure/src/main/java/org/springblossom/core/secure/config/SessionManager.java
@@ -8,17 +8,18 @@ import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.apache.shiro.web.util.WebUtils;
 import org.springblossom.core.launch.constant.TokenConstant;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.stereotype.Component;
+import org.springblossom.core.tool.utils.Func;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 
 /**
  * 自定义session管理
  * 支持从header或cookie中获取session id
  * 用于处理仅使用header token时下载文件无法鉴权的问题
+ *
  * @author guolf
  */
 public class SessionManager extends DefaultWebSessionManager {
@@ -26,7 +27,11 @@ public class SessionManager extends DefaultWebSessionManager {
 	@Override
 	protected Serializable getSessionId(ServletRequest request, ServletResponse response) {
 		// 优先从请求头获取Token
-		String token = WebUtils.toHttp(request).getHeader(TokenConstant.HEADER);
+		HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+		String token = httpServletRequest.getHeader(TokenConstant.HEADER);
+		if (Func.isBlank(token)) {
+			token = httpServletRequest.getParameter(TokenConstant.HEADER);
+		}
 		// 判断是否有值
 		if (StringUtils.isNoneBlank(token)) {
 			// 设置当前session状态
